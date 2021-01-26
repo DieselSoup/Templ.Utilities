419ca3669fd9177483a9e55c31ba54035c276e81
dieselsoupsolutions@gmail.com <dieselsoupsolutions@gmail.com>
2021-01-26 15:35:06 -0600

    Readding classes to source control

---
+++ Templ.Utilities.Helpers/Managers/DevicePermissionManager.cs
@@ -0,0 +1,84 @@
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Templ.Utilities.Managers
+{
+    public class DevicePermissionManager
+    {
+
+        private class RequestPermissionQueueItem
+        {
+            public Permission Permission { get; set; }
+            public Action<Dictionary<Permission, PermissionStatus>> OnPermissionResultCallBack { get; set; }
+        }
+
+        public static DevicePermissionManager Instance = new DevicePermissionManager();
+
+        private List<RequestPermissionQueueItem> permissionsToBeRequested = new List<RequestPermissionQueueItem>();
+        private bool isBusy;
+        private int currentPermissionRequest = -1;
+
+        private DevicePermissionManager()
+        {
+
+        }
+
+        [Obsolete]
+        private void ExecuteNextPendingPermissionRequests()
+        {
+            RequestPermissionAsync(permissionsToBeRequested[currentPermissionRequest].Permission,
+                permissionsToBeRequested[currentPermissionRequest].OnPermissionResultCallBack);
+
+        }
+
+        [Obsolete]
+        public async void RequestPermissionAsync(Permission permission, Action<Dictionary<Permission, PermissionStatus>> onPermissionResult)
+        {
+
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(permission);
+            if (status == PermissionStatus.Granted)
+            {
+                Dictionary<Permission, PermissionStatus> result = new Dictionary<Permission, PermissionStatus>
+            {
+                { permission, PermissionStatus.Granted }
+            };
+                onPermissionResult?.Invoke(result);
+                return;
+            }
+
+            if (isBusy)
+            {
+                RequestPermissionQueueItem requestPermissionQueueItem = new RequestPermissionQueueItem
+                {
+                    Permission = permission,
+                    OnPermissionResultCallBack = onPermissionResult
+                };
+                permissionsToBeRequested.Add(requestPermissionQueueItem);
+                return;
+            }
+
+            isBusy = true;
+            Dictionary<Permission, PermissionStatus> results = await CrossPermissions.Current.RequestPermissionsAsync(permission);
+            onPermissionResult?.Invoke(results);
+            isBusy = false;
+
+            currentPermissionRequest++;
+            if (currentPermissionRequest < permissionsToBeRequested.Count)
+            {
+
+                ExecuteNextPendingPermissionRequests();
+
+            }
+            else
+            {
+                currentPermissionRequest = -1;
+                permissionsToBeRequested = new List<RequestPermissionQueueItem>();
+            }
+
+        }
+    }
+}
